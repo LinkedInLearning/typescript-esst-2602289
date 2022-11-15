78474699a012a22d6316ef4dc784283a45292b48
David <info@activenode.de>
2022-11-15 21:23:43 +0100

    objects

---
+++ 04_01/objects.ts
@@ -0,0 +1,20 @@
+export function Demo() {
+  type AuthorObject = {
+    author: string;
+    address: {
+      city: string;
+      streetNumber: number;
+    };
+    hobbies: string[];
+  };
+  let person: AuthorObject[] = [
+    {
+      author: "David Lorenz",
+      address: {
+        city: "Wondercity",
+        streetNumber: 700,
+      },
+      hobbies: ["Tennis"],
+    },
+  ];
+}
