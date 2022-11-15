15f73161b4a785ed093f2b9afba2dc5d3eeef786
David <info@activenode.de>
2022-11-15 20:41:24 +0100

    03_03 fixed types and or-pipe

---
+++ 03_03/demo.ts
@@ -0,0 +1,9 @@
+export function Demo() {
+  let DARK_MODE: false = false;
+
+  const isDarkMode: boolean = DARK_MODE;
+
+  let errorCode: "ERROR_404" | "ERROR_500" | "ERROR_503";
+
+  errorCode = "ERROR_500";
+}
