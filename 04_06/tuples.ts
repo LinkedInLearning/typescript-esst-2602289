284176578ee5700a1b2d68094bd8c569dc407c20
David <info@activenode.de>
2022-11-16 08:04:17 +0100

    tuples added

---
+++ 04_06/tuples.ts
@@ -0,0 +1,6 @@
+export function Demo() {
+  type StringOrNumberArray = (string | number)[];
+  const myArray1: StringOrNumberArray = ["foo", 2, "bar"];
+
+  const xyTuple: [number, number, string] = [100, 140, "ich bin ein string"];
+}
