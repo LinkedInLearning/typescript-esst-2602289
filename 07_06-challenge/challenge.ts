21095e3cf3f5c1e97dc24351514f57f0e5e3cb56
David <info@activenode.de>
2022-11-24 09:56:10 +0100

    update

---
+++ 07_06-challenge/challenge.ts
@@ -0,0 +1,9 @@
+const ComplexFunction = () => {
+  return () => {
+    return [2, { courseName: "TypeScript" }] as const;
+  };
+};
+
+export function Challenge() {
+  type ComplexFunctionType = typeof ComplexFunction;
+}
